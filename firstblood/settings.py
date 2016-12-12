a7d570995d80f4461f648d5a37cbb861f479d778
mathorlee.li <mathorlee.li@fuwo.com>
2016-12-12 12:00:56 +0800

    save

--- firstblood/settings.py
+++ firstblood/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'firstblood.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': '123',
+        'HOST': '',
+        'PORT': 3306,
     }
 }
 
