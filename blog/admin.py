144df0ad1d1824852095424e0ad44e8594839ad8
mathorlee.li <mathorlee.li@fuwo.com>
2017-01-23 16:32:50 +0800

    save

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Article, Author, Tag
 
-admin.site.register([Article, Author, Tag])
+class ArticalAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Article, ArticalAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'qq', 'addr', 'email')
+    list_display_links = ()
+    list_filter = ()
+#     list_select_related = True
+    list_per_page = 10
+    list_editable = ()
+    search_fields = ('name', 'qq', 'email')
+admin.site.register(Author, AuthorAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Tag, TagAdmin)
